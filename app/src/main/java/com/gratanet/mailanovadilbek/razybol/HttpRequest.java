53b4ffbbc44080e2a93aabde3b39ecca039d04fb
adilbek12 <mv.adilbek@gmail.com>
2018-09-15 12:52:48 +0600

    First

---
+++ app/src/main/java/com/gratanet/mailanovadilbek/razybol/HttpRequest.java
@@ -0,0 +1,28 @@
+package com.gratanet.mailanovadilbek.razybol;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
+
+public class HttpRequest {
+  private RequestQueue requestQueue;
+
+  public HttpRequest(Context context) {
+    this.requestQueue = Volley.newRequestQueue(context);
+  }
+
+  public void send(int method, String url, final Map<String, String> params, Response.Listener<String> onResponse, Response.ErrorListener onError) {
+    StringRequest stringRequest = new StringRequest(method, url, onResponse, onError) {
+      @Override
+      protected Map<String, String> getParams() {
+        return params;
+      }
+    };
+    requestQueue.add(stringRequest);
+  }
+}
